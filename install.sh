7eb472c44925f221bc4c62f1b7d2896f31379855
TheRose20 <cybrenok91@mail.ru>
2025-01-26 21:03:37 +0300

    start bios support

--- install.sh
+++ install.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-# Only Efi install
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo "Exitst script with sudo!"
+  echo "Please run this script with sudo!"
   exit 1
 fi
 
-echo -n "Add password for root (default: password):"
-read -s PASSWORD
+read -p "Enter root password (default: password): " -s PASSWORD
 PASSWORD=${PASSWORD:-password}
-
-DISK="/dev/sda"
-HOSTNAME="archlinux"
-USERNAME="user"
-
-BASEPACKAGES_FILE="packs/base.packs"
-STARTPACKAGES_FILE="packs/start.packs"
+read -p "Enter disk to install on (default: /dev/sda): " DISK
+DISK=${DISK:-/dev/sda}
+read -p "Enter hostname (default: archlinux): " HOSTNAME
+HOSTNAME=${HOSTNAME:-archlinux}
+read -p "Enter username (default: user): " USERNAME
+USERNAME=${USERNAME:-user}
 
 PACMAN_CONF="/etc/pacman.conf"
 PARALLEL_DOWNLOADS=30
@@ -24,28 +21,40 @@ sed -i "s/^#ParallelDownloads = [0-50]*/ParallelDownloads = $PARALLEL_DOWNLOADS/
 sed -i "s/^#Color/Color/" "$PACMAN_CONF"
 
 if grep -q "^ParallelDownloads = $PARALLEL_DOWNLOADS" "$PACMAN_CONF"; then
-  echo "Pacman edited"
+  echo "Pacman configuration updated."
 else
-  echo "Error pacman.conf"
+  echo "Error updating pacman configuration."
   exit 1
 fi
 
 wipefs -a "$DISK"
 sgdisk -Z "$DISK"
 
-sgdisk -n 1:0:+500M -t 1:ef00 -c 1:"EFI" "$DISK"
-sgdisk -n 2:0:0 -t 2:8300 -c 2:"Linux" "$DISK"
+echo "Detecting boot mode..."
+if [[ -d /sys/firmware/efi ]]; then
+  echo "EFI mode detected."
+  sgdisk -n 1:0:+500M -t 1:ef00 -c 1:"EFI" "$DISK"
+  sgdisk -n 2:0:0 -t 2:8300 -c 2:"Linux" "$DISK"
 
-mkfs.fat -F32 "${DISK}1"
-mkfs.ext4 "${DISK}2"
+  mkfs.fat -F32 "${DISK}1"
+  mkfs.ext4 "${DISK}2"
 
-mount "${DISK}2" /mnt
-mkdir -p /mnt/boot
-mount "${DISK}1" /mnt/boot
+  mount "${DISK}2" /mnt
+  mkdir -p /mnt/boot
+  mount "${DISK}1" /mnt/boot
+else
+  echo "BIOS mode detected."
+  sgdisk -n 1:0:0 -t 1:8300 -c 1:"Linux" "$DISK"
 
-pacstrap /mnt base linux linux-firmware base-devel zsh networkmanager grub efibootmgr
+  mkfs.ext4 "${DISK}1"
+
+  mount "${DISK}1" /mnt
+fi
+
+pacstrap /mnt base linux linux-firmware base-devel zsh networkmanager grub
 
 genfstab -U /mnt >> /mnt/etc/fstab
+
 arch-chroot /mnt bash -c "
   echo '$HOSTNAME' > /etc/hostname
   ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
@@ -59,13 +68,15 @@ arch-chroot /mnt bash -c "
   echo '$USERNAME:$PASSWORD' | chpasswd
   echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
 
-  grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-  grub-mkconfig -o /boot/grub/grub.cfg
+  if [[ -d /sys/firmware/efi ]]; then
+    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+  else
+    grub-install --target=i386-pc $DISK
+  fi
 
+  grub-mkconfig -o /boot/grub/grub.cfg
   systemctl enable NetworkManager
 "
 
-#umount -R /mnt
-echo "Installation succes!"
-
-arch-chroot /mnt bash
+umount -R /mnt
+echo "Installation complete! Reboot to enjoy your new system."
